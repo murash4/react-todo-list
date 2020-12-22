75cffb9ac16046483068ebdaa466c732edaa1ae2
murash4 <qoc306a>
2020-12-23 00:02:13 +0300

    create todo-list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoList from './Todo/TodoList'
+import React from 'react'
+import Context from './context'
 
 function App() {
+  const [todos, setTodos] = React.useState([
+    {
+      id: 1,
+      completed: true,
+      title: 'Купить хлеб'
+    },
+    {
+      id: 2,
+      completed: false,
+      title: 'Купить масло'
+    },
+    {
+      id: 3,
+      completed: false,
+      title: 'Купить молоко'
+    }
+  ])
+
+  function toggleTodo (id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+
+        return todo
+      })
+    )
+  }
+
+  function removeTodo (id) {
+    setTodos(
+      todos.filter(todo => todo.id !== id)
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Provider value={{ removeTodo }}>
+      <div className='wrapper'>
+        <h1>React tutorial</h1>
+
+        {
+          todos.length ? <TodoList todos={ todos } onToggle={ toggleTodo } /> : <p>No Todos</p>
+        }
+      </div>
+    </Context.Provider>
   );
 }
 
